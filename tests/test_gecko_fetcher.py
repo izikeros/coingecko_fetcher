6b3faea11b49e1e355d1a5b0d21b23acf4fce8ea
Krystian Safjan <ksafjan@gmail.com>
2021-09-15 13:00:15 +0200

    Reformat code with black

--- tests/test_gecko_fetcher.py
+++ tests/test_gecko_fetcher.py
@@ -17,7 +17,9 @@ class TestGetCoingeckoFrontPage:
     def test_get_coingecko_front_page__list_len_correct(self):
         expected = self.n_entries * self.pages
         actual = len(self.resp)
-        assert actual == expected, f"response has a wrong length {actual} instead of {expected}"
+        assert (
+            actual == expected
+        ), f"response has a wrong length {actual} instead of {expected}"
 
     def test_get_coingecko_front_page__list_element_is_dict(self):
         assert isinstance(self.resp[0], dict), "element of the list is not a dict"
